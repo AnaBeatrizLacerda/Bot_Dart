d509fa742a8d1693c65103ee7cf6a95f4c97d476
araujonnicole <nicole.araujo250212@gmail.com>
2026-05-23 13:15:05 +0000

    message

--- bin/bot_dart.dart
+++ bin/bot_dart.dart
@@ -5,88 +5,85 @@ import 'package:dotenv/dotenv.dart';
 void main() async {
   final env = DotEnv()..load();
   final token = env['TELEGRAM_TOKEN'];
-
   print("Bot iniciado...");
-
-  final url = Uri.parse(
-    "https://api.telegram.org/bot$token/getUpdates",
+  int updateId = 0;
+  while (true) {
+ final url = Uri.parse(
+  "https://api.telegram.org/bot$token/getUpdates?offset=$updateId",
   );
-
   final request = await HttpClient().getUrl(url);
-
   final response = await request.close();
-
   final body = await response.transform(utf8.decoder).join();
-
   final data = jsonDecode(body);
-
+  if (data["result"].isEmpty) {
+  await Future.delayed(Duration(seconds: 2));
+  continue;
+}
   final ultimoUpdate = data["result"].last;
+  updateId = ultimoUpdate["update_id"] + 1;
 
-  print(ultimoUpdate);
+final mensagem = ultimoUpdate["message"]["text"];
+final chatId = ultimoUpdate["message"]["chat"]["id"];
 
-  // pega o texto enviado pelo usuário
-  final mensagem = ultimoUpdate["message"]["text"];
+String resposta;
 
-  print("CPF recebido: $mensagem");
-
-  // valida o CPF
+if (mensagem == "/start") {
+  resposta =
+      "Olá! 👋\n"
+      "Eu sou um bot validador de CPF.\n"
+      "Envie um CPF para validação.";
+} else {
   final cpfValido = validarCPF(mensagem);
 
-  // mostra resultado
   if (cpfValido) {
-    print("CPF válido!");
+    resposta = "✅ CPF válido!";
   } else {
-    print("CPF inválido!");
+    resposta = "❌ CPF inválido!";
   }
 }
+print(resposta);
+final sendUrl = Uri.parse(
+  "https://api.telegram.org/bot$token/sendMessage?chat_id=$chatId&text=$resposta",
+);
 
-// função que valida CPF
+final sendRequest = await HttpClient().getUrl(sendUrl);
+
+await sendRequest.close();
+await Future.delayed(Duration(seconds: 2));
+}
+}
 bool validarCPF(String cpf) {
-
-  // remove pontos e traços
   cpf = cpf.replaceAll(RegExp(r'[^0-9]'), '');
 
-  // verifica tamanho
-  if (cpf.length != 11) {
-    return false;
-  }
+  if (cpf.length != 11) return false;
 
-  // evita CPF com números repetidos
-  if (RegExp(r'^(\d)\1*$').hasMatch(cpf)) {
-    return false;
-  }
+  if (RegExp(r'^(\d)\1*$').hasMatch(cpf)) return false;
 
   int soma = 0;
 
-  // cálculo do primeiro dígito
   for (int i = 0; i < 9; i++) {
     soma += int.parse(cpf[i]) * (10 - i);
   }
 
-  int resto = (soma * 10) % 11;
+  int primeiroDigito = (soma * 10) % 11;
 
-  if (resto == 10) {
-    resto = 0;
-  }
+  if (primeiroDigito == 10) primeiroDigito = 0;
 
-  if (resto != int.parse(cpf[9])) {
+  if (primeiroDigito != int.parse(cpf[9])) {
     return false;
   }
 
   soma = 0;
 
-  // cálculo do segundo dígito
   for (int i = 0; i < 10; i++) {
     soma += int.parse(cpf[i]) * (11 - i);
   }
 
-  resto = (soma * 10) % 11;
+  int segundoDigito = (soma * 10) % 11;
 
-  if (resto == 10) {
-    resto = 0;
-  }
+  if (segundoDigito == 10) segundoDigito = 0;
 
-  if (resto != int.parse(cpf[10])) {
+  if (segundoDigito != int.parse(cpf[10])) {
     return false;
   }
 
